e7d1b1c6c2ccb27e7ca18e215cf95ddf27515d3d
Armandolumano <armandomanuellumano@gmail.com>
2026-04-27 23:34:19 +0100

    Add files via upload
    classe pessoa

---
+++ PESSOA.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using gestao_escolar.modelos;
+namespace gestao_escolar.modelos
+{
+    public class PESSOA
+    { 
+    public string nome { get; set; }
+    public string dataNascimento { get; set; }
+    public string telefone { get; set; }
+
+   
+    public PESSOA(string nome, string dataNascimento, string telefone)
+    {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+    }
+
+   
+    public string getNome() 
+    { 
+        return this.nome; 
+    }
+    
+    public int getIdade() 
+    {
+        DateTime dt;
+        if (DateTime.TryParse(this.dataNascimento, out dt))
+        {
+            return DateTime.Now.Year - dt.Year;
+        }
+        return 0;
+
+
+
+    }
+    }
+}
+
